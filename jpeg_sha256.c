d5aa32e7ad5acf662ca3cc7115387245a589a30e
Rurik Bugdanov <rurik.bugdanov@alsenet.com>
2016-01-28 23:56:20 +0700

    Update jpeg_sha256
    - Exit if not a regular file (could work on fifo and stdin using fread instead of
      fseek in those cases)
    - Exit if does not look like a jpeg file
    - Copy faster
    - Process a file list

--- jpeg_sha256.c
+++ jpeg_sha256.c
@@ -30,29 +30,102 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
+#include <libgen.h>
 
 #include "sha256sum.h"
 
 #define VERSION "1.00"
 
-int jpeg_strip(char *filename);
+int jpeg_strip(FILE *f);
+int print_jpeg_sha256(FILE *f);
+int isRegularFile(char *filename);
+FILE *isJpegFile(char *filename);
+char *appName;
 
-char *filename;
+#define BUFSIZE 32767
+char *buf;
 
 int mypipe[2];
+struct stat sb;
+unsigned char header[6];
+char *filename;
 
 int main(int argc, char **argv) {
 
-  int pid;
   int err=0;
 
+  appName=basename(argv[0]);
+
   if (argc<2 || !strcmp(argv[1],"-h")) {
-    fprintf(stderr,"usage: jpeg_sha256 [-h] <jpeg_file>\n");
+    fprintf(stderr,"usage: %s [-h] <jpeg_file> [<jpeg_file> ...]\n",appName);
     exit(1);
   }
 
-  filename=argv[1];
+  buf=malloc(BUFSIZE);
+  if (!buf) {
+    fprintf(stderr,"%s: out of memory !\n",appName);
+    exit(1);
+  }
+
+  FILE *f;
+  for (int i=1; i<argc; ++i) {
+    filename=argv[i];
+    if ((f=isJpegFile(filename))) {
+      err|=print_jpeg_sha256(f);
+    } else {
+      err=1;
+    }
+  }
+  exit(err);
+}
+
+int isRegularFile(char *filename) {
+  stat(filename, &sb);
+
+  if (S_ISREG(sb.st_mode)) {
+    return 1;
+  }
+
+  if (S_ISDIR(sb.st_mode)) {
+    fprintf(stderr,"%s: %s: is a directory\n",appName,filename);
+  } else {
+    fprintf(stderr,"%s: %s: is not a regular file\n",appName,filename);
+  }
+
+  return 0;
+}
+
+FILE *isJpegFile(char *filename) {
+
+  if (!isRegularFile(filename)) {
+    return 0;
+  }
+
+  FILE *f=fopen(filename,"r");
+  if (!f) {
+    fprintf(stderr,"%s: cannot open %s\n",appName,filename);
+    return 0;
+  }
+
+  size_t count=fread(header,1,6,f);
+  if (count!=6 || header[0]!=0xff && header[1]!=0xd8 && header[2]!=0xff && header[3]&0xfe!=0xe0) {
+    fprintf(stderr,"%s: %s: not a jpeg\n",appName,filename);
+    fclose(f);
+    return 0;
+  }
+
+  return f;
+
+}
+
+
+int print_jpeg_sha256(FILE *f) {
+
+  int pid;
+  int err;
+
   pipe(mypipe);
 
   if (!(pid=fork())) {
@@ -60,7 +133,7 @@ int main(int argc, char **argv) {
     close(mypipe[0]);
     close(mypipe[1]);
 
-    err=jpeg_strip(filename);
+    err=jpeg_strip(f);
     exit(err);
 
   } else {
@@ -74,39 +147,52 @@ int main(int argc, char **argv) {
       for (int i=0; i<SHA256_DIGEST_LENGTH; ++i) {
         printf("%02x",hash[i]);
       }
-      printf("\n");
+      printf(" %s\n", filename);
     }
-    exit(err);
+    return err;
   }
 
 }
 
-int jpeg_strip(char *filename) {
+int jpeg_strip(FILE *f) {
 
-  FILE *f=fopen(filename,"r");
-  if (!f) {
-    fprintf(stderr,"error: cannot open file %s\n",filename);
-    return errno;
+  int c;
+  unsigned char d;
+  int lsb=0;
+  int length;
+  int err=0;
+
+  const unsigned char *dontStrip="\xc0\xc1\xc2\xc3\xc5\xc6\xc7\xc9\xca\xcb\xcd\xce\xcf\xdd";
+
+  if (putchar(0xff)!=0xff || putchar(0xd8)!=0xd8) {
+    fprintf(stderr,"%s: stdout: write error\n",appName);
+    return 1;
   }
 
-  int c; 
-  int lsb=0;
-  int err=0;
-  int first=1;
+  // skip first app segment
+  length=header[4]<<8;
+  length+=header[5];
 
-  const unsigned char *dontStrip="\xc0\xc1\xc2\xc3\xc5\xc6\xc7\xc9\xca\xcb\xcd\xce\xcf\xd8\xd9\xda\xdd";
+  if (length<2) {
+    fprintf(stderr,"%s: %s: invalid segment length\n",appName,filename);
+    return 1;
+  }
+
+  if (length>2 && fseek(f,length-2,SEEK_CUR)<0) {
+    err=errno;
+    fprintf(stderr,"%s: %s: seek failed\n",appName,filename);
+    return err;
+  }
+
 
   while ((c=fgetc(f))!=EOF) {
 
     if (lsb) {
 
-      int length;
-      int d;
-
-      if (c==0xd8 || c==0xd9 || c==0xda) { // SOI or EOI or SOS
+      if (c==0xd9 || c==0xda) { // EOI or SOS
         // copy
         if (putchar(0xff)!=0xff || putchar(c)!=c) {
-          fprintf(stderr,"stdout: write error\n");
+          fprintf(stderr,"%s: stdout: write error\n",appName);
           err=1;
           break;
         }
@@ -115,41 +201,56 @@ int jpeg_strip(char *filename) {
         }
 
         if (c==0xda) {  // SOS
-          while ((c=fgetc(f))!=EOF) {
-            d=c;
-            if (putchar(d)!=d) {
-              fprintf(stderr,"stdout: write error\n");
+          size_t count, wcount, written;
+
+          // copy the remaining of file
+          while ((count=fread(buf,1,BUFSIZE,f))) {
+            d=buf[count-1];
+            wcount=0;
+            while(wcount<count && ((written=fwrite(buf+wcount,1,count-wcount,stdout)))) {
+              wcount+=written;
+            }
+            if (wcount!=count) {
+              fprintf(stderr,"%s: stdout: write error\n",appName);
               err=1;
               break;
             }
           }
-          if (d!=0xd9) {
-            fprintf(stderr,"%s: last byte is not EOI\n",filename);
+
+          if (ferror(f)) {
+            fprintf(stderr,"%s: %s: read error\n",appName,filename);
             err=1;
+
+          } else {
+            if (d!=0xd9) {
+              fprintf(stderr,"%s: %s: last byte %02x is not EOI\n",appName,filename,d);
+              err=1;
+            }
           }
           break;
         }
 
       } else {
+
         // get segment length
         length=0;
 
         if ((d=fgetc(f))==EOF) {
-          fprintf(stderr,"%s: unexpected end of file\n",filename);
+          fprintf(stderr,"%s: %s: unexpected end of file\n",appName,filename);
           err=1;
           break;
         }
         length+=d<<8;
 
         if ((d=fgetc(f))==EOF) {
-          fprintf(stderr,"%s: unexpected end of file\n",filename);
+          fprintf(stderr,"%s: %s: unexpected end of file\n",appName,filename);
           err=1;
           break;
         }
         length+=d;
 
         if (length<2) {
-          fprintf(stderr,"%s: invalid segment length\n",filename);
+          fprintf(stderr,"%s: %s: invalid segment length\n",appName,filename);
           err=1;
           break;
         }
@@ -157,7 +258,7 @@ int jpeg_strip(char *filename) {
         // copy everything but jfif, exif, xmp, comments, iptc, and unknown markers
         if (strchr(dontStrip,c)) {
           if (putchar(0xff)!=0xff || putchar(c)!=c || putchar(d>>8)!=d>>8 || putchar(d&0xff)!=d&0xff) {
-            fprintf(stderr,"stdout: write error\n");
+            fprintf(stderr,"%s: stdout: write error\n",appName);
             err=1;
             break;
           }
@@ -165,14 +266,14 @@ int jpeg_strip(char *filename) {
           length-=2;
           while(length && ((c=fgetc(f))!=EOF)) {
             if (putchar(c)!=c) {
-              fprintf(stderr,"stdout: write error\n");
+              fprintf(stderr,"%s: stdout: write error\n",appName);
               err=1;
               break;
             }
             --length;
           }
           if (length && !err) {
-            fprintf(stderr,"%s: unexpected end of file\n",filename);
+            fprintf(stderr,"%s: %s: unexpected end of file\n",appName,filename);
             err=1;
           }
           if (err) {
@@ -182,17 +283,23 @@ int jpeg_strip(char *filename) {
         } else {
           // skip segment
           if (length>2 && fseek(f,length-2,SEEK_CUR)<0) {
-            fprintf(stderr,"%s: seek failed\n",filename);
-            err=errno;                         
-            break;                             
+            fprintf(stderr,"%s: %s: seek failed\n",appName,filename);
+            err=errno;
+            break;
           }
-        }                                    
+        }
       }
       lsb=0;
 
     } else if (c==0xff) {
       lsb=1;
+
+    } else {
+      fprintf(stderr,"%s: %s: not a jpeg\n",appName,filename);
+      err=1;
+      break;
     }
+
   }
 
   fclose(f);
